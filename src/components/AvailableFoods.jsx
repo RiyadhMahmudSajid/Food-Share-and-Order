4f0f0104cf9ca3202b39f638502b3923a6af61a9
RiyadhMahmudSajid <rdhsajid@gmail.com>
2025-06-12 12:39:39 +0600

    apply tanstack/react-query

--- src/components/AvailableFoods.jsx
+++ src/components/AvailableFoods.jsx
@@ -1,50 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 
 import AvailableFoodCard from './AvailableFoodCard';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchFoods = async ({ queryKey }) => {
+    const [_key, search, sort] = queryKey;
+    const res = await fetch(`http://localhost:3000/foods?searchParams=${search}&sort=${sort}`)
+    return res.json()
+}
+
+
+
 
 const AvailableFoods = () => {
-    const [search,setSearch] = useState("")
-    const [sort,setSort] = useState("")
-    const [foodData,setFoodData] = useState([])
-    const [three,setThree] = useState(true)
-    useEffect(()=>{
-        fetch(`http://localhost:3000/foods?searchParams=${search}&sort=${sort}`)
-        .then((res) =>res.json())
-        .then((data)=>setFoodData(data))
-    },[search, sort])
+    const [search, setSearch] = useState("")
+    const [sort, setSort] = useState("")
+    
+    const [three, setThree] = useState(true)
+
    
+     
+    const {data : foodData = [] , isLoading} = useQuery({
+        queryKey  : ['foods',search , sort],
+        queryFn: fetchFoods
+    })
+  
+
     const availableFoods = foodData.filter(food => food.FoodStatus === "available")
 
-    
+
     return (
         <div className='max-w-6xl mx-auto p-4'>
             <div className='mb-6'>
                 <input type="text"
-                 placeholder="Warning" 
-                 onChange={(e) => setSearch(e.target.value)}
-                 className="input input-warning" />
-                 <div className='flex gap-2'>
-                    <button className='btn btn-secondary' onClick={()=>setSort('asc')}>Sort ASC</button>
-                    <button  className='btn btn-secondary' onClick={()=>setSort('desc')}>Sort DESC</button>
-                    <button className='btn btn-accent' onClick={()=>setThree(!three) }>
+                    placeholder="Warning"
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="input input-warning" />
+                <div className='flex gap-2'>
+                    <button className='btn btn-secondary' onClick={() => setSort('asc')}>Sort ASC</button>
+                    <button className='btn btn-secondary' onClick={() => setSort('desc')}>Sort DESC</button>
+                    <button className='btn btn-accent' onClick={() => setThree(!three)}>
                         Change Layout
                     </button>
-                 </div>
-            </div>
-           {
-             availableFoods.length > 0 ? (
-             
-                <div className={`grid grid-cols-1 ${three ? 'md:grid-cols-3' : 'md:grid-cols-2'} gap-4 md:gap-14 `}>
-                    {
-                        availableFoods.map(availableFood => (
-                            <AvailableFoodCard key={availableFood._id} food = {availableFood} ></AvailableFoodCard>
-                        ))
-                    }
                 </div>
-             ) : (
-                <p className='text-center text-gray-500'>No available food found</p>
-             )
-           }
+            </div>
+            {
+                isLoading ? (<p>Loading...</p>) :
+                availableFoods.length > 0 ? (
+
+                    <div className={`grid grid-cols-1 ${three ? 'md:grid-cols-3' : 'md:grid-cols-2'} gap-4 md:gap-14 `}>
+                        {
+                            availableFoods.map(availableFood => (
+                                <AvailableFoodCard key={availableFood._id} food={availableFood} ></AvailableFoodCard>
+                            ))
+                        }
+                    </div>
+                ) : (
+                    <p className='text-center text-gray-500'>No available food found</p>
+                )
+            }
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,23 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import router from './routes/router'
 import AuthProvider from './provider/AuthProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 
 
 
 
-
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router}>  </RouterProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router}>  </RouterProvider>
 
-    </AuthProvider>
+      </AuthProvider>
+
+
+    </QueryClientProvider>
+
   </StrictMode>,
 )
